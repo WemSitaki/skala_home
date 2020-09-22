89f1104de9746bbf50d3f925f065bd3c0a8ca09b
WemSitaki <mir.skala@gmail.com>
2020-09-22 14:36:30 +0200

    dsadas

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  apps : [{
-    name: 'skala_home',
-    script: 'build/app.js',
-    env: {
-        "COMMON_VARIABLE": "true"
-      },
-    watch: '.'
-  }],
+    apps: [{
+        name: 'skala_home',
+        script: 'build/app.js',
+        env: {
+            "COMMON_VARIABLE": "true"
+        },
+        watch: '.'
+    }],
 
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : '192.168.0.220',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : './app',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    deploy: {
+        production: {
+            //key: "~/.ssh/id_rsa",
+            user: 'ubuntu',
+            host: '192.168.0.220',
+            ref: 'origin/master',
+            //repo : 'git@github.com:WemSitaki/skala_home.git',
+            repo: 'https://github.com/WemSitaki/skala_home.git',
+            path: '/home/ubuntu/app',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
     }
-  }
 };
